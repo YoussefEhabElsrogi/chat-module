f5bde36b7441f5e73f6837b8ffeae43ab130bd3b
Youssef Elsrogi <youssefelsorgi@gmail.com>
2025-09-23 23:38:02 +0300

    clean: Remove Docker files and simplify deployment

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Storage;
 
 Route::get('/', function () {
     return redirect()->route('dashboard.register');
 });
-
-// Serve static assets
-Route::get('/assets/{path}', function ($path) {
-    $filePath = public_path('assets/' . $path);
-
-    if (file_exists($filePath)) {
-        $mimeType = mime_content_type($filePath);
-        return response()->file($filePath, [
-            'Content-Type' => $mimeType,
-            'Cache-Control' => 'public, max-age=31536000'
-        ]);
-    }
-
-    abort(404);
-})->where('path', '.*');
